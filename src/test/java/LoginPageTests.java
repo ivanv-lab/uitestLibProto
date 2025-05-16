fb9ea45d24379a9b35f5e75757bf8ac6abac03c6
ivanv-lab <ivanvozisov33@yandex.ru>
2025-05-16 15:46:26 +0300

    Initial Commit

--- src/test/java/LoginPageTests.java
+++ src/test/java/LoginPageTests.java
@@ -14,6 +14,7 @@ public class LoginPageTests {
     SelenideElement passwordInput=$x(".//input[@id='password']");
     SelenideElement submitButton=$x(".//button[@id='button_auth']");
     SelenideElement alert=$x(".//div[contains(@class,'alert')]/span");
+    SelenideElement changeLanguageButton=$x(".//div[label[@for='language']]//input");
 
     @BeforeEach
     void goToAddress(){
@@ -65,12 +66,25 @@ public class LoginPageTests {
     @Test
     @Description("Проверка смены языка на Английский и Русский")
     void changeLanguage(){
+        changeLanguageButton.click();
+        $x(".//button[div/span[contains(.,'English')]]").click();
+        submitButton.click();
+        alert.shouldHave(text("It is impossible to log in.\n" +
+                "Incorrect login / password."));
 
+        changeLanguageButton.click();
+        $x(".//button[div/span[contains(.,'Русский')]]").click();
+        submitButton.click();
+        alert.shouldHave(text("Невозможно авторизоваться.\n" +
+                "Неправильный логин/пароль."));
     }
 
     @Test
     @Description("Проверка успешной авторизации")
     void successLogin(){
-
+        loginInput.sendKeys("admin@admin.com");
+        passwordInput.sendKeys("Admin");
+        submitButton.click();
+        $x(".//span[@class='top-user-link']").shouldHave(text("admin@admin.com"));
     }
 }
