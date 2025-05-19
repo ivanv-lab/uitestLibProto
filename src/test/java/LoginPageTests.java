30ee7186a35f62f99bbdb28d0882a83c6eb8ac1f
ivanv-lab <ivanvozisov33@yandex.ru>
2025-05-19 10:16:16 +0300

    йцу

--- src/test/java/LoginPageTests.java
+++ src/test/java/LoginPageTests.java
@@ -1,20 +1,19 @@
-import com.codeborne.selenide.*;
+import com.codeborne.selenide.Configuration;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import testlib.base.BasePage;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LoginPageTests {
+public class LoginPageTests extends BasePage {
 
-    SelenideElement loginInput=$x(".//input[@id='login']");
-    SelenideElement passwordInput=$x(".//input[@id='password']");
-    SelenideElement submitButton=$x(".//button[@id='button_auth']");
-    SelenideElement alert=$x(".//div[contains(@class,'alert')]/span");
-    SelenideElement changeLanguageButton=$x(".//div[label[@for='language']]//input");
+    By loginInput=By.xpath(".//input[@id='login']");
+    By passwordInput=By.xpath(".//input[@id='password']");
+    By submitButton=By.xpath(".//button[@id='button_auth']");
+    By changeLanguageButton=By.xpath(".//div[label[@for='language']]//input");
 
     @BeforeAll
     static void setup(){
@@ -31,63 +30,102 @@ public class LoginPageTests {
     @Test
     @Description("Только логин")
     void loginOnly(){
-        loginInput.sendKeys("admin@admin.com");
-        submitButton.click();
-        alert.shouldHave(text("Невозможно авторизоваться.\n" +
-                "Неправильный логин/пароль."));
+//        loginInput.sendKeys("admin@admin.com");
+//        submitButton.click();
+//        alert.shouldHave(text("Невозможно авторизоваться.\n" +
+//                "Неправильный логин/пароль."));
+
+        sendKeys(loginInput,"admin@admin.com");
+        click(submitButton);
+        assertEquals(getAlertText(),"Невозможно авторизоваться.\n" +
+                "Неправильный логин/пароль.");
     }
 
     @Test
     @Description("Только пароль")
     void passwordOnly(){
-        passwordInput.sendKeys("Admin");
-        submitButton.click();
-        alert.shouldHave(text("Невозможно авторизоваться.\n" +
-                "Неправильный логин/пароль."));
+//        passwordInput.sendKeys("Admin");
+//        submitButton.click();
+//        alert.shouldHave(text("Невозможно авторизоваться.\n" +
+//                "Неправильный логин/пароль."));
+
+        sendKeys(passwordInput,"Admin");
+        click(submitButton);
+        assertEquals(getAlertText(),"Невозможно авторизоваться.\n" +
+                "Неправильный логин/пароль.");
     }
 
     @Test
     @Description("Неверный логин")
     void invalidLogin(){
-        loginInput.sendKeys("qweqwe");
-        passwordInput.sendKeys("Admin");
-        submitButton.click();
-        alert.shouldHave(text("Невозможно авторизоваться.\n" +
-                "Неправильный логин/пароль."));
+//        loginInput.sendKeys("qweqwe");
+//        passwordInput.sendKeys("Admin");
+//        submitButton.click();
+//        alert.shouldHave(text("Невозможно авторизоваться.\n" +
+//                "Неправильный логин/пароль."));
+
+        sendKeys(loginInput,"qweqwe");
+        sendKeys(passwordInput,"Admin");
+        click(submitButton);
+        assertEquals(getAlertText(),"Невозможно авторизоваться.\n" +
+                "Неправильный логин/пароль.");
     }
 
     @Test
     @Description("Неверный пароль")
     void invalidPassword(){
-        loginInput.sendKeys("admin@admin.com");
-        passwordInput.sendKeys("qweqwe");
-        submitButton.click();
-        alert.shouldHave(text("Невозможно авторизоваться.\n" +
-                "Неправильный логин/пароль."));
+//        loginInput.sendKeys("admin@admin.com");
+//        passwordInput.sendKeys("qweqwe");
+//        submitButton.click();
+//        alert.shouldHave(text("Невозможно авторизоваться.\n" +
+//                "Неправильный логин/пароль."));
+
+        sendKeys(loginInput,"admin@admin.com");
+        sendKeys(passwordInput,"qweqwe");
+        click(submitButton);
+        assertEquals(getAlertText(),"Невозможно авторизоваться.\n" +
+                "Неправильный логин/пароль.");
     }
 
     @Test
     @Description("Проверка смены языка на Английский и Русский")
     void changeLanguage(){
-        changeLanguageButton.click();
-        $x(".//button[div/span[contains(.,'English')]]").click();
-        submitButton.click();
-        alert.shouldHave(text("It is impossible to log in.\n" +
-                "Incorrect login / password."));
+//        changeLanguageButton.click();
+//        $x(".//button[div/span[contains(.,'English')]]").click();
+//        submitButton.click();
+//        alert.shouldHave(text("It is impossible to log in.\n" +
+//                "Incorrect login / password."));
+//
+//        changeLanguageButton.click();
+//        $x(".//button[div/span[contains(.,'Русский')]]").click();
+//        submitButton.click();
+//        alert.shouldHave(text("Невозможно авторизоваться.\n" +
+//                "Неправильный логин/пароль."));
 
-        changeLanguageButton.click();
-        $x(".//button[div/span[contains(.,'Русский')]]").click();
-        submitButton.click();
-        alert.shouldHave(text("Невозможно авторизоваться.\n" +
-                "Неправильный логин/пароль."));
+        click(changeLanguageButton);
+        click(By.xpath(".//button[div/span[contains(.,'English')]]"));
+        click(submitButton);
+        assertEquals(getAlertText(),"It is impossible to log in.\n" +
+                "Incorrect login / password.");
+
+        click(changeLanguageButton);
+        click(By.xpath(".//button[div/span[contains(.,'Русский')]]"));
+        click(submitButton);
+        assertEquals(getAlertText(),"Невозможно авторизоваться.\n" +
+                "Неправильный логин/пароль.");
     }
 
     @Test
     @Description("Проверка успешной авторизации")
     void successLogin(){
-        loginInput.sendKeys("admin@admin.com");
-        passwordInput.sendKeys("Admin");
-        submitButton.click();
-        $x(".//span[@class='top-user-link']").shouldHave(text("admin@admin.com"));
+//        loginInput.sendKeys("admin@admin.com");
+//        passwordInput.sendKeys("Admin");
+//        submitButton.click();
+//        $x(".//span[@class='top-user-link']").shouldHave(text("admin@admin.com"));
+
+        sendKeys(loginInput,"admin@admin.com");
+        sendKeys(passwordInput,"Admin");
+        click(submitButton);
+        assertEquals(getCurrentUrl(),"http://192.168.128.195/acui/dashboard");
     }
 }
