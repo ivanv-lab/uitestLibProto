89437e433134bf1f75b0ce31065f702baeeb2798
ivanv-lab <ivanvozisov33@yandex.ru>
2025-05-30 12:30:15 +0300

    Работает, но нужно самому все чистить и allure не туда пишет результаты

--- test-orchestrator/run_tests.sh
+++ test-orchestrator/run_tests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo Начало работы скрипта
-/app/gradlew clean test allureReport
-echo Запустил gradlew
-/opt/allure/bin/allure serve /app/build/allure-results
+/app/gradlew test allureReport
+echo gradlew отработал
+#/opt/allure/bin/allure serve --port 35277 /app/build/allure-results
+allure generate /app/build/allure-results -o /app/allure-report
+while true; do
+  # Здесь могут быть какие-то действия, которые нужно выполнять периодически
+  # Например, проверка состояния системы, логирование, и т.п.
+  echo "Скрипт работает..."
+  sleep 60  # Пауза в 60 секунд (1 минута)
+done
 echo Закончил работу
