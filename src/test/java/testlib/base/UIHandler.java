f3eda86a938d416171929d2d9f1c3412608027c7
ivanv-lab <ivanvozisov33@yandex.ru>
2025-06-17 17:51:40 +0300

    Далее следует перенести необходимые методы из pages в UIHandler, допилить работу с кэшем, сделать SQLHandler

---
+++ src/test/java/testlib/base/UIHandler.java
@@ -0,0 +1,112 @@
+package testlib.base;
+
+public class UIHandler extends BasePage{
+
+    public UIHandler loginAcui(){
+
+    }
+
+    public UIHandler loginLkui(){
+
+    }
+
+    public UIHandler login(){
+
+    }
+
+    public UIHandler logout(){
+
+    }
+
+    public UIHandler sectionClick(String section){
+
+    }
+
+    public UIHandler subSectionClick(String section, String subSection){
+
+    }
+
+    public UIHandler buttonClick(){
+
+    }
+
+    public UIHandler inputClick(){
+
+    }
+
+    public UIHandler inputSet(){
+
+    }
+
+    public UIHandler dropDownListInputSet(){
+
+    }
+
+    public UIHandler dropDownListValuesContainsValue(String dropDownList, String value){
+
+    }
+
+    public UIHandler radioButtonOn(){
+
+    }
+
+    public UIHandler radioButtonOff(){
+
+    }
+
+    public UIHandler checkBoxOn(){
+
+    }
+
+    public UIHandler checkBoxOff(){
+
+    }
+
+    public UIHandler setInputValueFromTableValues(String inputName){
+
+    }
+
+    public UIHandler filterSet(String filterInput, String value){
+
+    }
+
+    public UIHandler settingsOff(String... settingsToOff){
+
+    }
+
+    public UIHandler settingsOn(String... settingsToOn){
+
+    }
+
+    public UIHandler calendarSet(){
+
+    }
+
+    public UIHandler tableRowExists(){
+
+    }
+
+    public UIHandler tableRowNotExists(){
+
+    }
+
+    public UIHandler tableRowCellClick(){
+
+    }
+
+    public UIHandler tableCellHrefClick(){
+
+    }
+
+    public UIHandler tagExists(){
+
+    }
+
+    public UIHandler tagNotExists(){
+
+    }
+
+    public UIHandler deleteFromTableIfExists(){
+
+    }
+}
