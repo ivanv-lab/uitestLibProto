ab5e85bd511a12c1f124a183ccc2b4e3128d983b
ivanv-lab <ivanvozisov33@yandex.ru>
2025-06-23 10:15:03 +0300

    Реализовал Allure Step, возможно нужно будет реализовать тестирование через Actions. Необходимо реализовать Init заполнение следующим образом: 10% UI, 90% - API, возможно нужно будет изменить в дальнейшем

--- src/test/java/testlib/base/BasePage.java
+++ src/test/java/testlib/base/BasePage.java
@@ -1,6 +1,7 @@
 package testlib.base;
 
 import com.codeborne.selenide.*;
+import io.qameta.allure.Allure;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import testlib.utils.handlers.PropertyHandler;
@@ -19,71 +20,92 @@ public abstract class BasePage{
             .getProperty("timeout.default"));
 
     public SelenideElement find(By locator){
-        waitForElementConditions(locator,defaultTimeout,visible);
-        return $(locator);
+       return Allure.step("Поиск элемента с локатором: "+locator,()-> {
+            waitForElementConditions(locator, defaultTimeout, visible);
+            return $(locator);
+        });
     }
 
     public List<String> findCollection(By locator){
-        waitForElementConditions(locator,defaultTimeout,visible,clickable,interactable);
-        return $$(locator)
-                .asFixedIterable()
-                .stream().map(SelenideElement::getText)
-                .collect(Collectors.toList());
+        return Allure.step("Поиск коллекции элементов",()-> {
+            waitForElementConditions(locator, defaultTimeout, visible, clickable, interactable);
+            return $$(locator)
+                    .asFixedIterable()
+                    .stream().map(SelenideElement::getText)
+                    .collect(Collectors.toList());
+        });
     }
 
     public void click(By locator) {
-        waitForElementConditions(locator,defaultTimeout,visible,clickable);
-        retry(()->{
-            find(locator).shouldBe(enabled).click();
-            return null;
+        Allure.step("Клик по элементу с локатором: "+locator,()-> {
+            waitForElementConditions(locator, defaultTimeout, visible, clickable);
+            retry(() -> {
+                find(locator).shouldBe(enabled).click();
+                return null;
+            });
         });
     }
 
     public void sendKeys(By locator, String text) {
-        waitForElementConditions(locator,defaultTimeout,visible,clickable,interactable);
-        find(locator).sendKeys(Keys.CONTROL+"A");
-        find(locator).sendKeys(Keys.BACK_SPACE);
-        find(locator).sendKeys(text);
+        Allure.step("Ввод текста "+text+" в элемент с локатором "+locator,()-> {
+            waitForElementConditions(locator, defaultTimeout, visible, clickable, interactable);
+            find(locator).sendKeys(Keys.CONTROL + "A");
+            find(locator).sendKeys(Keys.BACK_SPACE);
+            find(locator).sendKeys(text);
+        });
     }
 
     public String getText(By locator) {
-        waitForElementConditions(locator,defaultTimeout,visible,clickable);
-        return find(locator).getText();
+        return Allure.step(String.format("Получение текста из элемента с локатором "+locator),()-> {
+            waitForElementConditions(locator, defaultTimeout, visible, clickable);
+            return find(locator).getText();
+        });
     }
 
     public String getValue(By locator) {
-        waitForElementConditions(locator,defaultTimeout,visible,clickable);
-        return find(locator).getValue();
+        return Allure.step(String.format("Получение значения из элемента с локатором "+locator),()-> {
+            waitForElementConditions(locator, defaultTimeout, visible, clickable);
+            return find(locator).getValue();
+        });
     }
 
     private void waitForElementConditions(By locator,int timeout,WebElementCondition... conditions){
         retry(()->{
             for(WebElementCondition condition:conditions){
-            $(locator).shouldBe(condition,Duration.ofSeconds(timeout));
+                Allure.step(String.format("Проверка элемента на условия: "+condition),()-> {
+                    $(locator).shouldBe(condition, Duration.ofSeconds(timeout));
+                });
         }
             return null;
         });
     }
 
     public void login(String login,String password){
-        sendKeys(By.xpath(".//input[@id='login']"),login);
-        sendKeys(By.xpath(".//input[@id='password']"),password);
-        click(By.xpath(".//button[@id='button_auth']"));
+        Allure.step(String.format("Логин на странице авторизации с логином "+login+" и паролем "+password),()-> {
+            sendKeys(By.xpath(".//input[@id='login']"), login);
+            sendKeys(By.xpath(".//input[@id='password']"), password);
+            click(By.xpath(".//button[@id='button_auth']"));
+        });
     }
 
     public void changeLanguageToRus(){
-        click(By.xpath(".//div[label[@for='language']]//input"));
-        click(By.xpath(".//button[div/span[contains(.,'Русский')]]"));
+        Allure.step("Изменение языка интерфейса на русский",()-> {
+            click(By.xpath(".//div[label[@for='language']]//input"));
+            click(By.xpath(".//button[div/span[contains(.,'Русский')]]"));
+        });
     }
 
     public String getAlertText() {
-        By alertLocator=By.xpath(".//div[contains(@class,'alert')]/span");
-        waitForElementConditions(alertLocator,defaultTimeout,visible,clickable);
-        return find(alertLocator).getText();
+       return Allure.step("Получение текста уведомления или ошибки",()-> {
+            By alertLocator = By.xpath(".//div[contains(@class,'alert')]/span");
+            return find(alertLocator).getText();
+        });
     }
 
     public void openSidebar(){
-        find(By.xpath(".//div[contains(@class,'sidebar')]")).hover();
-        click(By.xpath(".//button[@id='minimizeSidebar']"));
+        Allure.step("Открытие сайдбара (навигационного меню)",()-> {
+            find(By.xpath(".//div[contains(@class,'sidebar')]")).hover();
+            click(By.xpath(".//button[@id='minimizeSidebar']"));
+        });
     }
 }
